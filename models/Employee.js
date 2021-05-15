faa796541eb14bbe39456fe17d18967cdeaaeeca
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-05-15 10:13:48 +0700

    [Js]: Define employee prototies

---
+++ models/Employee.js
@@ -0,0 +1,26 @@
+function Employee() {
+  this.code = "";
+  this.name = "";
+  this.title = "";
+  this.salary = "";
+  this.timeWork = "";
+  this.indexTitle = "";
+  this.handleCaculateSalary = function () {
+    var salary = this.salary * this.indexTitle;
+  };
+  this.handleArrangeLevel = function () {
+    var level = "";
+
+    if (this.timeWork >= 120) {
+      level = "Xuất sắc";
+    } else if (100 <= this.timeWork && this.timeWork < 120) {
+      level = "Giỏi";
+    } else if (75 <= this.timeWork && this.timeWork < 100) {
+      level = "Khá";
+    } else {
+      level = "Trung bình";
+    }
+
+    return level;
+  };
+}
